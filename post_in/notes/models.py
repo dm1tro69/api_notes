26795004c2f3a8b8ab0f4c8bb2f75e86679bd17d
dmytro69 <dimolg22@gmail.com>
2019-11-12 22:34:24 +0200

    add_note

---
+++ post_in/notes/models.py
@@ -0,0 +1,16 @@
+from django.db.models import (Model, CharField, TextField, DateTimeField)
+
+# Create your models here.
+
+class Note(Model):
+    title = CharField(max_length=255)
+    text = TextField(blank=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['updated']
+
